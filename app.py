3caada27a733ac6bb731c4cf16f51d014daccace
Aarati Khamakar <60148173+aarati0122@users.noreply.github.com>
2022-06-22 12:36:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,19 @@ from sklearn.model_selection import GridSearchCV
 from sklearn import svm
 from io import BytesIO
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.metrics import plot_confusion_matrix
 
+import warnings
+warnings.filterwarnings("ignore")
 
 def main():
   # model =  pickle.load(open('img_model.p','rb'))
